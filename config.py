563b750301be1b108eaef542f7a5e2285863e1bc
RexAshh <95988728+Rexashh@users.noreply.github.com>
2023-02-13 00:44:16 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("9143931"))
+API_HASH = getenv("5dcaf3b395762de3172651527214da4d")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("6054586380:AAGh0oYvdxUU1cI13JHcj_UG4RIZq4AOJZs")
