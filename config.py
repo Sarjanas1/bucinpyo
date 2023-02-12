2f231782c7a9d7385a99499ed55439e13c440e24
RexAshh <95988728+Rexashh@users.noreply.github.com>
2023-02-13 01:44:19 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-SUDO_USER = list(
+SUDO_USERS = list(
     map(int, getenv("SUDO_USER", "").split())
 )  # Input type must be interger
