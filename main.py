ced30953e7d3aed4cbe049503c80910953ed3251
RexAshh <95988728+Rexashh@users.noreply.github.com>
2023-02-13 11:40:48 +0700

    test button

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+# IMPORT BUKAN PYRO
 import asyncio
 from sys import version as pyver
 
+# IMPORT CONFIG
+import config
+
+# IMPORT PYRO
 import pyrogram
 from pyrogram import Client, filters
 from pyrogram import __version__ as pyrover
 from pyrogram import filters, idle
 from pyrogram.errors import FloodWait
 from pyrogram.types import Message
-
-import config
-
 from pyrogram import Client, filters
+from pyrogram import InlineKeyboardButton, InlineKeyboardMarkup 
 
 app = pyrogram.Client(
     "RexaBot",
@@ -19,6 +22,23 @@ app = pyrogram.Client(
     bot_token=config.BOT_TOKEN,
 )
 
+# COMMAND START AND HELP
+
+START_MESSAGE = " HALO SAYA BOT CONFESS BUATAN REXA"
+START_BUTTON = [
+    [InlineKeyboardButton('REXA', url='https://t.me/JustRex')]
+]
+
+@app.on_message(filters.command("start") & filter.private)
+def start(app, message):
+    text = START_MESSAGE
+    reply_markup = InlineKeyboardMarkup(START_BUTTON)
+    message.reply(
+        text=text,
+        reply_markup=reply_markup,
+        disable_web_page_preview=True
+    )
+# REGEX CARI NAMA
 @app.on_message(filters.regex("jean"))
 @app.on_message(filters.regex("jeann"))
 def regex(app, message):
@@ -36,7 +56,7 @@ def regex(app, message):
 def regex(app, message):
     app.send_message(message.chat.id,"nahhhh ini bener !! hehehehe ")
 
-
+# REGEX SEND MEDIA
 @app.on_message(filters.regex("foto"))
 def regex(app, message):
     app.send_photo(message.chat.id,"https://graph.org/file/6fd592fa2e0cc9ecc07f7.jpg")
